2c362a1b2afcae359b1efeb83da41fb1caedf5a9
vigneshvm506 <vigneshvm506@gmail.com>
2025-01-01 21:09:57 +0530

    Add frontend folder

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="py-1 bg-dark">
+      <p className="text-center text-white mt-1 ">
+        VKYcart - 2025 All Rights Reserved
+      </p>
+    </footer>
+  );
+}
